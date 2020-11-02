1232d25246e223bf3764e8f26127d6b80c0e3465
VirtualyMichael <71190633+VirtualyMichael@users.noreply.github.com>
2020-11-02 17:00:40 -0600

    Add files via upload

---
+++ Talk As Bot.py
@@ -0,0 +1,4 @@
+import dhooks
+from dhooks import Webhook
+hook = Webhook('https://discord.com/api/webhooks/759919362139619338/F4VMrtFoSYAmBfMWzOwKcDiMkd1mTsauVS5hj2ZCmouePw0pwlxLgyMPeziXi1OuvZi9')
+hook.send("your text here")
